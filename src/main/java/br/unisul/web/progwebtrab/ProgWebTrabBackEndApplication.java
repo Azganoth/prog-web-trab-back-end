1d5c87f06f92990489896b2a55be71ef69567092
Ademir Ferreira <ademir.ferreiracif@gmail.com>
2019-05-16 01:37:33 -0300

    Initial commit

---
+++ src/main/java/br/unisul/web/progwebtrab/ProgWebTrabBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.web.progwebtrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgWebTrabBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgWebTrabBackEndApplication.class, args);
+    }
+
+}
